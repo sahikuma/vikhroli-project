6844635a3d756c215f9a2ebf4716bc611383411a
sahikuma <sahil.a.kumar@capgemini.com>
2018-11-19 17:11:10 +0530

    First Commit

---
+++ src/main/java/movies/spring/data/neo4j/SampleMovieApplication.java
@@ -0,0 +1,16 @@
+package movies.spring.data.neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableNeo4jRepositories("movies.spring.data.neo4j.repositories")
+public class SampleMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleMovieApplication.class, args);
+    }
+} 
---
+++ src/main/java/movies/spring/data/neo4j/controller/MovieController.java
@@ -0,0 +1,38 @@
+package movies.spring.data.neo4j.controller;
+
+import java.util.Collection;
+import java.util.Map;
+
+import movies.spring.data.neo4j.domain.Movie;
+import movies.spring.data.neo4j.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MovieController {
+
+	private final MovieService movieService;
+	
+	public MovieController(MovieService movieService) {
+		this.movieService = movieService;
+	}
+
+    @GetMapping("/movie")
+    public Movie findByTitle(@RequestParam String title) {
+        return movieService.findByTitle(title);
+    }
+
+    @GetMapping("/movies")
+    public Collection<Movie> findByTitleLike(@RequestParam String title) {
+        return movieService.findByTitleLike(title);
+    }
+
+    @GetMapping("/graph")
+	public Map<String, Object> graph(@RequestParam(value = "limit",required = false) Integer limit) {
+		return movieService.graph(limit == null ? 100 : limit);
+	}
+}
